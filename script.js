e33d6893b180fc5f0f897915250f74c36b21dd40
diyanshu0360 <diyanshu0360@gmail.com>
2023-04-29 14:55:55 +0530

    Final Project Done

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 const digits = document.querySelectorAll("p");
 let userDate = prompt("Enter Date in Below Format {Wed Apr 26 2023 14:54:00}");
-if(!userDate){
-    alert("Enter Date");
-}
-else{
+if (userDate) {
     const endDate = new Date(userDate);
-    
+
     const myfunc = () => {
         const startDate = new Date();
-        
+
         let diff = (endDate - startDate) / 1000;
-        
+
         const day = Math.floor(diff / (24 * 60 * 60));
         diff = diff % (24 * 60 * 60);
         const hour = Math.floor(diff / (60 * 60));
@@ -18,7 +15,7 @@ else{
         const minutes = Math.floor(diff / (60));
         diff = diff % 60;
         const second = Math.floor(diff);
-        
+
         if (diff < 0) {
             let mainbox = document.getElementById('mainb');
             mainbox.innerHTML = "";
